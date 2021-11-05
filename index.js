3261cc594c88679aafd60906bd0430e7ed31d439
Andrew <andrew.spon@gmail.com>
2021-11-05 13:50:04 +0000

    update fetch function

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const {fetchMyIP} = require('./iss.js');
 
+fetchMyIP((error, ip) => {
+  if(error) {
+    console.log('It didn\'t work!', error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:', ip);
+});
+
--- iss.js
+++ iss.js
@@ -1,4 +1,10 @@
+const request = require('request')
 const fetchMyIP = function(callback) {
+  request('https://api.ipify.org/?format=json' , (error, body, response) => {
+    console.log('error', error);
+    console.log('statusCode:', response && response);
+    console.log('body', body);
+  });
   //use request to fetch IP address from JSON API
 };
 
