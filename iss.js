ef75690d546744e2baa13ae432cf45ee71b37e9b
Andrew <andrew.spon@gmail.com>
2021-11-05 22:33:27 +0000

    fix request to grab ip address

--- iss.js
+++ iss.js
@@ -1,11 +1,17 @@
 const request = require('request')
 const fetchMyIP = function(callback) {
-  request('https://api.ipify.org/?format=json' , (error, body, response) => {
-    console.log('error', error);
-    console.log('statusCode:', response && response);
-    console.log('body', body);
+  request('https://api.ipify.org/?format=json' , (error, JSON, response) => {
+    // console.log('error', error);
+    // console.log('statusCode:', response && response);
+    console.log('JSON', JSON.body);
   });
   //use request to fetch IP address from JSON API
 };
 
+request('https://api.ipify.org/?format=json' , (error, JSON, response) => {
+    // console.log('error', error);
+    // console.log('statusCode:', response && response);
+    console.log('JSON', JSON.body);
+  });
+// console.log(fetchMyIP);
 module.exports = {fetchMyIP};
