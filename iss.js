4d311ec75a396ce79f46a3adbe2898f62ba939a9
Andrew <andrew.spon@gmail.com>
2021-11-06 16:33:28 +0000

    clean up files

--- iss.js
+++ iss.js
@@ -1,27 +1,23 @@
-const request = require('request')
+const request = require('request');
 const fetchMyIP = function(callback) {
   request('https://api.ipify.org/?format=json' , (error, json, response) => {
     if (error) {
       callback(error);
-      return
+      return;
     }
+    
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${json}`;
+      callback(Error(msg), null);
+      return;
+    }
+
     try {
       callback(null, JSON.parse(json.body).ip); // .IP so only ouput IP isntead of object
     } catch (error) {
       callback(new Error('invalid JSON'));
     }
-    // console.log('error', error);
-    // console.log('statusCode:', response && response);
-    // console.log('JSON', JSON.body);
-    
   });
-  //use request to fetch IP address from JSON API
 };
 
-// request('https://api.ipify.org/?format=json' , (error, JSON, response) => {
-//     // console.log('error', error);
-//     // console.log('statusCode:', response && response);
-//     console.log('JSON', JSON.body);
-// });
-// console.log(fetchMyIP());
-module.exports = {fetchMyIP}; 
+module.exports = {fetchMyIP};
