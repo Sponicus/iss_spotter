fc20db02078751b5c4fa341ccf87c8d7b54d0c3e
Andrew <andrew.spon@gmail.com>
2021-11-06 16:26:00 +0000

    fix errors w/ mentor

--- iss.js
+++ iss.js
@@ -1,17 +1,27 @@
 const request = require('request')
 const fetchMyIP = function(callback) {
-  request('https://api.ipify.org/?format=json' , (error, JSON, response) => {
+  request('https://api.ipify.org/?format=json' , (error, json, response) => {
+    if (error) {
+      callback(error);
+      return
+    }
+    try {
+      callback(null, JSON.parse(json.body).ip); // .IP so only ouput IP isntead of object
+    } catch (error) {
+      callback(new Error('invalid JSON'));
+    }
     // console.log('error', error);
     // console.log('statusCode:', response && response);
-    console.log('JSON', JSON.body);
+    // console.log('JSON', JSON.body);
+    
   });
   //use request to fetch IP address from JSON API
 };
 
-request('https://api.ipify.org/?format=json' , (error, JSON, response) => {
-    // console.log('error', error);
-    // console.log('statusCode:', response && response);
-    console.log('JSON', JSON.body);
-  });
-// console.log(fetchMyIP);
-module.exports = {fetchMyIP};
+// request('https://api.ipify.org/?format=json' , (error, JSON, response) => {
+//     // console.log('error', error);
+//     // console.log('statusCode:', response && response);
+//     console.log('JSON', JSON.body);
+// });
+// console.log(fetchMyIP());
+module.exports = {fetchMyIP}; 
