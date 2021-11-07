b3026219a07c7fba5b4017643a54c599ec3ee71c
Andrew <andrew.spon@gmail.com>
2021-11-07 18:53:44 +0000

    implement fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {fetchMyIP} = require('./iss');
 const {fetchCoordsByIP} = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -13,3 +14,16 @@ fetchMyIP((error, ip) => {
 //   // console.log(error);
 //   console.log(data);
 // });
+
+
+///////////////// dont understand implementation /////////////
+const exampleCoords = { latitude: '49.27670', longitude: '-123.13000' };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:' , passTimes);
+});
--- iss.js
+++ iss.js
@@ -39,4 +39,28 @@ const fetchCoordsByIP = (data, callback) => {
 
   });
 };
+
+//// could not implement on my own ////
+const fetchISSFlyOverTimes = function(coords, callback) {
+  const url = `https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching ISS pass times: ${body}`), null);
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
+
+
+
 module.exports = {fetchMyIP,fetchCoordsByIP};
