9602c016d2dd81983338f38f14b980729fc924aa
Andrew <andrew.spon@gmail.com>
2021-11-07 18:33:01 +0000

    add nextISSTimesForMyLocation

--- index2.js
+++ index2.js
@@ -1,15 +1,22 @@
-const {fetchMyIP} = require('./iss_promised');
-const {fetchCoordsByIP} = require('./iss_promised');
-const {fetchISSFlyOverTimes} = require('./iss_promised');
+// const {fetchMyIP} = require('./iss_promised');
+// const {fetchCoordsByIP} = require('./iss_promised');
+// const {fetchISSFlyOverTimes} = require('./iss_promised');
 
-fetchMyIP()
-  .then(body => console.log(body));
+// fetchMyIP()
+//   .then(body => console.log(body));
 
-  fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(body => console.log(body));
+//   fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(body => console.log(body));
 
-  fetchMyIP()
-  .then(fetchCoordsByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => console.log(body));
+//   fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
