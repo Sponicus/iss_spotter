73406201ad2cc8a516a3b787b3f079aff82f74ad
Andrew <andrew.spon@gmail.com>
2021-11-07 18:28:54 +0000

    implement all three functions

--- iss_promised.js
+++ iss_promised.js
@@ -4,4 +4,20 @@ const fetchMyIP = () => {
   return request('https://api.ipify.org?format=json');
 }; 
 
-module.exports = {fetchMyIP};
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+module.exports = {fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
